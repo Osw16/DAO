08aa55c276b3b48d490bee22c118e11731edc937
osw16 <oswaldoj16@gmail.com>
2022-05-19 04:06:24 -0500

    first commit

---
+++ scripts/2-deploy-drop.js
@@ -0,0 +1,38 @@
+import { AddressZero } from "@ethersproject/constants";
+import sdk from "./1-initialize-sdk.js";
+import { readFileSync } from "fs";
+
+(async () => {
+  const TreasuryAddress = '0x3eAb1a20D8104F830D0C21248d679F246c5f5743';
+  try {
+    const editionDropAddress = await sdk.deployer.deployEditionDrop({
+      // The collection's name, ex. CryptoPunks
+      name: "Fanaty Membership",
+      // A description for the collection.
+      description: "A DAO for fans",
+      // The image that will be held on our NFT! The fun part :).
+      image: readFileSync("scripts/assets/support.jpg"),
+      // We need to pass in the address of the person who will be receiving the proceeds from sales of nfts in the contract.
+      // We're planning on not charging people for the drop, so we'll pass in the 0x0 address
+      // you can set this to your own wallet address if you want to charge for the drop.
+      primary_sale_recipient: TreasuryAddress,
+    });
+
+    // this initialization returns the address of our contract
+    // we use this to initialize the contract on the thirdweb sdk
+    const editionDrop = sdk.getEditionDrop(editionDropAddress);
+
+    // with this, we can get the metadata of our contract
+    const metadata = await editionDrop.metadata.get();
+
+    console.log(
+      "✅ Successfully deployed editionDrop contract, address:",
+      editionDropAddress,
+    );
+    console.log("✅ editionDrop metadata:", metadata);
+  } catch (error) {
+    console.log("failed to deploy editionDrop contract", error);
+  }
+})();
+
+//node scripts/2-deploy-drop.js
